c6a08010b4b0ac425d3b1cc7080b88cbcd22bdca
Jose Nieto <ton-email@exemple.com>
2025-05-24 20:14:11 +0200

    fix: add CORS header to GET /get-distribution

--- index.cjs
+++ index.cjs
@@ -58,8 +58,7 @@ function extractPostal(addr) {
 }
 
 function setCors(res) {
-  res.setHeader('Access-Control-Allow-Origin',
-                'https://decoration.ams.v6.pressero.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
