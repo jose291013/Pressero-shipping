c22d0d833953a414e17d2f7763e10201b7c03284
Jose Nieto <ton-email@exemple.com>
2025-05-25 12:01:05 +0200

    fix(webhook): use correct ServiceName for Pressero

--- index.cjs
+++ index.cjs
@@ -258,7 +258,7 @@ if (req.method === 'POST' && req.url === '/webhook') {
 
     // 4) **On renvoie un tableau** contenant votre méthode “External”  
     const method = {
-      ServiceName:  'Livraison multi-adresses', // libellé dans le select
+      ServiceName:  'External', // libellé dans le select
       ServiceCode:  'External',                 // valeur <option>
       Carrier:      carrier,
       TotalCost:    parseFloat(totalCost.toFixed(2)),
